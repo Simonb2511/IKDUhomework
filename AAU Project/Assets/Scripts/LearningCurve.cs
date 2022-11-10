895c382766a3f6eda0fee3d8f862fb9b1fde2b24
Simonb2511 <Simonb251@gmail.com>
2022-11-10 01:29:38 +0100

    Script

---
+++ AAU Project/Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    private int CurrentLevel = 1;
+
+    private int addedLevel = 1;
+    public string Name;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowCharacterInfo();
+        AddLv();
+
+
+    }
+
+    void ShowCharacterInfo()
+    {
+        Debug.Log("What is the name of character?");
+        Debug.Log(Name);
+        Debug.LogFormat("The character {0} has current level = {1}", Name, CurrentLevel);
+
+
+
+    }
+
+    void AddLv()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CurrentLevel = CurrentLevel + addedLevel;
+            Debug.Log(CurrentLevel);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        AddLv();
+
+    }
+}
